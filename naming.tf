1185256d25e948285a92c45b3e98e96cee36e46c
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-12-21 20:26:00 +0100

    Fix naming policy

--- naming.tf
+++ naming.tf
@@ -5,6 +5,6 @@
 resource "azurecaf_name" "function" {
   resource_types = ["azurerm_function_app", "general", "azurerm_resource_group", "azurerm_storage_account"]
   prefixes       = []
-  suffixes       = [var.purpose, var.purpose]
+  suffixes       = [var.purpose]
   clean_input    = true
 }
