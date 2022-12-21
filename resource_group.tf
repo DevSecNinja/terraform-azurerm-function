79471e6a99d1cbf50efd01c781a4dfff8183bb09
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-12-21 16:48:51 +0100

    Explicitly lock on RG

--- resource_group.tf
+++ resource_group.tf
@@ -3,6 +3,7 @@
 #
 
 resource "azurerm_resource_group" "function" {
+  #ts:skip=accurics.azure.NS.272 No lock necessary
   name     = azurecaf_name.function.results["azurerm_resource_group"]
   location = var.location
   tags     = var.tags
