0ca8f69b49e1b331e75929444b6de113bea2d97c
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-12-21 20:43:49 +0100

    Add appinsights

--- naming.tf
+++ naming.tf
@@ -3,7 +3,7 @@
 #
 
 resource "azurecaf_name" "function" {
-  resource_types = ["azurerm_function_app", "general", "azurerm_resource_group", "azurerm_storage_account"]
+  resource_types = ["azurerm_function_app", "general", "azurerm_resource_group", "azurerm_storage_account", "azurerm_application_insights"]
   prefixes       = []
   suffixes       = [var.purpose]
   clean_input    = true
